bdb65173edd35c691fc328c74defafbe7837ff49
MALWARE <notmalware@inbox.ru>
2024-04-02 16:37:50 +0300

    Добавлены проверки в лабораторной 2

--- Laba2.cs
+++ Laba2.cs
@@ -2,16 +2,22 @@ namespace RPM {
     public class Laba2 {
         public static void Run() {
             Console.Write("Введите x: ");
-            double x = Convert.ToDouble(Console.ReadLine());
-            if (x >= -1) {
-                Console.WriteLine("X должен быть < -1");
+            double x;
+            try {
+                x = Convert.ToDouble(Console.ReadLine());
+                if (x >= -1) throw new Exception();
+            } catch {
+                Console.WriteLine("Введите число X, который < -1!");
                 return;
             }
             Console.Write("Введите конечное значение n: ");
 
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            if (n2 <= 0) {
-                Console.WriteLine("N должен быть положительным");
+            int n2;
+            try {
+                n2 = Convert.ToInt32(Console.ReadLine());
+                if (n2 <= 0) throw new Exception();
+            } catch {
+                Console.WriteLine("Введите положительное число N!");
                 return;
             }
 
@@ -19,7 +25,7 @@ namespace RPM {
             for (int n = 0; n <= n2; n++) {
                 result += Math.Pow(-1, n + 1) / ((2 * n + 1) * Math.Pow(x, 2 * n + 1));
             }
-            Console.WriteLine(-Math.PI/2 + result);
+            Console.WriteLine(-Math.PI / 2 + result);
         }
     }
 }
